f445ea5e39ea75feeb42568e8420f32ba9fb302d
GyeomFka <clazziquai01@gmail.com>
2022-03-22 15:42:15 +0900

    09_격자판 최대합

--- src/_02/_07.java
+++ src/_02/_07.java
@@ -1,27 +1,46 @@
 package _02;
 
+import java.util.Scanner;
+
+/**
+ * 7. 점수계산
+ */
 public class _07 {
 	public static void main(String[] args) {
-		int[] input = {1, 0, 1, 1, 1, 0, 0, 0, 1, 1};
+		Scanner scan = new Scanner(System.in);
+		int cnt = scan.nextInt();
+		int[] input = new int[cnt];
+		for (int i = 0; i < cnt; i++) {
+			input[i] = scan.nextInt();
+		}
 
 		int answer = solution(input);
 		System.out.println(answer);
-
 	}
 
 	private static int solution(int[] input) {
 		int answer = 0;
-
-		for (int i = 0; i < input.length-1; i++) {
-			if (input[i] == 1) {
-				if (input[i+1] == 0) {
-					answer++;
+		int con = 1;
+		int a = 2;
+		for (int i = 0; i < input.length; i++) {
+			if(input[i] == 1) {
+				if (i == 0) {
+					answer += con;
+					continue;
+				}
+				if (input[i - 1] == 0) {
+					answer += con;
 				} else {
-
+					answer += a;
+					a++;
+				}
+			} else {
+				if(i == 0) continue;
+				if (input[i - 1] == 1) {
+					a = 2;
 				}
 			}
 		}
-
 		return answer;
 	}
 
