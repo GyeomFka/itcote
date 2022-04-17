e7bfc2ac321bcfc83702976a7c06d9b3a3ab1e50
GyeomFka <clazziquai01@gmail.com>
2022-04-17 18:01:18 +0900

    stack 후위연산

--- src/_05/_04_postfix.java
+++ src/_05/_04_postfix.java
@@ -6,6 +6,23 @@ import java.util.Stack;
 
 public class _04_postfix {
 	public static void main(String[] args) {
+		String str = "";
+		Stack<Integer> stack = new Stack<>();
+
+		for (char x : str.toCharArray()) {
+			if(Character.isDigit(x)) {
+				stack.push(x - 48);
+				// 0 -> 48 , 1 -> 49
+			} else {
+				int rt = stack.pop();
+				int lt = stack.pop();
+				if(x=='+') stack.push(lt+rt);
+				else if (x=='-') stack.push(lt-rt);
+				else if (x=='*') stack.push(lt*rt);
+				else if (x=='/') stack.push(lt/rt);
+			}
+		}
+		int answer = stack.get(0);
 
 	}
 }
